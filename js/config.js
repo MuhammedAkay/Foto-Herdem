9d4e8fc677f3afa136fdc99c0da0b13536f17f00
MuhammedAkay <106436149+MuhammedAkay@users.noreply.github.com>
2026-09-13 18:20:22 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
-// Supabase bağlantı bilgileri.
-// Supabase proje ayarları -> API bölümündeki Project URL ve anon public key'i yapıştırın.
 window.FH_CONFIG = {
   SUPABASE_URL: "https://jetmirrrbkybospagjgk.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_fLbNEIRUKhWbR7aB45-WJA_XnoNefNn"
+  SUPABASE_ANON_KEY: "sb_publishable_fLbNEIRUKhWbR7aB45-WJA_XnoNefNn",
+  ALBUMS_URL:
+    "https://raw.githubusercontent.com/MuhammedAkay/Foto-Herdem/main/Alb%C3%BCmler/albums.json",
+  PHOTO_URL:
+    "https://raw.githubusercontent.com/MuhammedAkay/Foto-Herdem/main/Alb%C3%BCmler/"
 };

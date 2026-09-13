0845d193744a13e67c97e8cbe7de5a3e9f896911
MuhammedAkay <106436149+MuhammedAkay@users.noreply.github.com>
2026-09-14 02:33:57 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
 window.FH_CONFIG = {
   SUPABASE_URL: "https://jetmirrrbkybospagjgk.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_fLbNEIRUKhWbR7aB45-WJA_XnoNefNn",
-  ALBUMS_URL:
-    "https://raw.githubusercontent.com/MuhammedAkay/Foto-Herdem/main/Alb%C3%BCmler/albums.json",
   PHOTO_URL:
     "https://raw.githubusercontent.com/MuhammedAkay/Foto-Herdem/main/Alb%C3%BCmler/"
 };
